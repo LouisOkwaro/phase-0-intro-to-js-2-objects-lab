69a7864ff8652f0b705e90ff46df37be4c2c284e
LouisOkwaro <okwarolouis@gmail.comgit config --global user.email okwarolouis@gmail.com>
2022-12-05 18:26:04 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your solution in this file!
+const employee = {name:"John", streetAddress:"19789 Groove"}
+
+function updateEmployeeWithKeyAndValue(employee,name,newName){
+     return{...employee,[name]:newName};
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, name, newName) {
+    employee[name]=newName;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, name) {
+    const newEmployee = {...employee};
+    delete newEmployee[name];
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee,name){
+    delete employee[name];
+    return employee;
+}
